bdbc635fe1719f59fb62f508fc7b30509a885f50
Cristobal Muñoz Barrios <crmunoz2018@udec.cl>
2025-03-30 20:41:24 -0300

    test: agregar prueba para saludar según la hora del día

--- kata-ohce/test_ohce.py
+++ kata-ohce/test_ohce.py
@@ -14,3 +14,10 @@ if __name__ == '__main__':
         ohce = Ohce("Cristobal")
         resultado = ohce.procesar("reconocer")  # Es un palíndromo
         self.assertIn("¡Bonita palabra!", resultado)
+    def test_saludo_según_la_hora(self):
+        def hora_falsa():
+            return 9  # 9 AM
+
+        ohce = Ohce("Cristobal", hora_actual=hora_falsa)
+        saludo = ohce.saludo()
+        self.assertEqual(saludo, "¡Buenos días, Cristobal!")
